1a805766998d446e40481b74f0494744a4a14a8a
Ben Hagan <benhagan@gmail.com>
2014-08-27 11:36:03 -0700

    Demographic Age Ranges

--- sample-queries.sql
+++ sample-queries.sql
@@ -21,3 +21,16 @@ FROM master m LEFT OUTER JOIN tags t ON t.interaction_id = m.interaction_id
 WHERE m.demographic_sex is not null
 GROUP BY t.label, m.demographic_sex
 ;
+
+/**
+ * Demographic Age Ranges
+ */
+SELECT
+	t.label,
+	CONCAT(m.demographic_age_range_start,' to ',m.demographic_age_range_end) AS age_range,
+	COUNT(CONCAT(m.demographic_age_range_start,' to ',m.demographic_age_range_end)) AS count
+FROM master m
+LEFT OUTER JOIN tags t ON t.interaction_id = m.interaction_id
+WHERE m.demographic_age_range_start IS NOT NULL
+GROUP BY t.label, age_range
+;
