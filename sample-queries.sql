34b49999a95469234e2dcc7582bb7d5d1c8229a4
Ben Hagan <benhagan@gmail.com>
2014-08-28 13:15:54 -0700

    Likes and interests - percentages

--- sample-queries.sql
+++ sample-queries.sql
@@ -76,11 +76,13 @@ order by age_range, t.label
  * Likes and interests - percentages
  */
 SELECT t.label, d.interest, COUNT(*), COUNT(*) / total_t.total_cnt as percentage
-FROM demographic_likes_and_interests d LEFT OUTER JOIN tags t ON t.interaction_id = d.interaction_id
-    INNER JOIN (SELECT label, COUNT(*) AS total_cnt
-                FROM tags
-                GROUP BY label
+FROM demographic_likes_and_interests d LEFT OUTER JOIN tags t ON d.interaction_id = t.interaction_id
+    INNER JOIN (SELECT tg.label, COUNT(*) AS total_cnt
+                FROM tags tg, demographic_likes_and_interests dl
+                where tg.interaction_id = dl.interaction_id
+                GROUP BY tg.label
     ) total_t ON total_t.label = t.label
 WHERE d.interest is not null
 GROUP BY t.label, d.interest
+order by t.label, percentage desc
 ;
