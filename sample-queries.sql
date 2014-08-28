4f4007512f6e764f2f43e1be7f681dd0ff74394a
Ben Hagan <benhagan@gmail.com>
2014-08-28 11:16:30 -0700

    Delete duplicate user data.

--- sample-queries.sql
+++ sample-queries.sql
@@ -5,16 +5,31 @@ CREATE INDEX master_interaction_id ON master (interaction_id);
 CREATE INDEX tags_interaction_id ON tags (interaction_id);
 CREATE INDEX demographic_likes_and_interests_interaction_id ON demographic_likes_and_interests (interaction_id);
 
-
 /**
  * Drop any duplicate user data from master
  */
 ALTER IGNORE TABLE `master` ADD UNIQUE INDEX (`twitter_id`);
 
 /**
+ * Delete duplicate user data from demographic_likes_and_interests
+ */
+DELETE FROM `demographic_likes_and_interests`
+WHERE `interaction_id` not in(
+	SELECT `interaction_id` FROM master
+);
+
+/**
+ * Delete duplicate user data from TAGS
+ */
+DELETE FROM `tags`
+WHERE `interaction_id` not in(
+	SELECT `interaction_id` FROM master
+);
+
+/**
  * Gender by volume
  */
-select t.label, m.demographic_sex, count(*)
+SELECT t.label, m.demographic_sex, count(*)
 FROM
     master m
 LEFT OUTER JOIN tags t ON t.interaction_id = m.interaction_id
@@ -55,4 +70,17 @@ where m.demographic_age_range_start is not null
 and  m.demographic_age_range_end is not null
 group by t.label, age_range
 order by age_range, t.label
+;
+
+/**
+ * Likes and interests - percentages
+ */
+SELECT t.label, d.interest, COUNT(*), COUNT(*) / total_t.total_cnt as percentage
+FROM demographic_likes_and_interests d LEFT OUTER JOIN tags t ON t.interaction_id = d.interaction_id
+    INNER JOIN (SELECT label, COUNT(*) AS total_cnt
+                FROM tags
+                GROUP BY label
+    ) total_t ON total_t.label = t.label
+WHERE d.interest is not null
+GROUP BY t.label, d.interest
 ;
